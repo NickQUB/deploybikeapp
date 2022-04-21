a9da4743b8d7471dd91c15418cc5fbed263b3edc
Nick Gaywood <ngaywood01@qub.ac.uk>
2022-04-21 22:23:47 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,51 @@
+
+
+# In[ ]:
+
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+# Connect to main app.py file
+from app import app
+from app import server
+
+# Connect to app pages
+from apps import gettoday, overall, stations, forecast
+# from apps import forecast
+
+
+
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content', children=[])
+])
+
+
+
+
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/apps/gettoday':
+        return gettoday.layout
+    if pathname == '/apps/overall':
+        return overall.layout
+    if pathname == '/apps/stations':
+        return stations.layout
+    if pathname == '/apps/forecast':
+        return forecast.layout
+    else:
+        return gettoday.layout
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+
